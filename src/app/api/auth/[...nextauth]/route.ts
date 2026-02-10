86c91b4311d574e4368137f987b6bb861c4531f8
Edun Yusuf <yusufedun19@gmail.com>
2026-02-10 16:08:34 +0100

    next-auth

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,44 @@
+import NextAuth, { type Session } from 'next-auth';
+import GitHubProvider from 'next-auth/providers/github';
+import { type JWT } from 'next-auth/jwt';
+
+declare module 'next-auth' {
+  interface Session {
+    accessToken?: string;
+  }
+}
+
+declare module 'next-auth/jwt' {
+  interface JWT {
+    accessToken?: string;
+  }
+}
+
+const handler = NextAuth({
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+      authorization: {
+        params: {
+          scope: 'read:user user:email repo',
+        },
+      },
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken as string;
+      return session;
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
